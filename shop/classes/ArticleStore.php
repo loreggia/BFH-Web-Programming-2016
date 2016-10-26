414126029ae17aea2955736eaa577953139151bc
kenboy192 <kenlestander@hotmail.com>
2016-10-26 17:34:41 +0200

    Sets hinzugefügt
    Sets werden nun auch angegeben.
    Zusätzlich werden Sets und Optionen gruppiert angegeben.

--- shop/classes/ArticleStore.php
+++ shop/classes/ArticleStore.php
@@ -18,6 +18,7 @@ class ArticleStore extends BaseStore
     {
         $article["attributes"] = $this->getArticleAttributes($article["article_id"]);
         $article["options"] = $this->getArticleOptions($article["article_id"]);
+		$article["sets"] = $this->getArticleSets($article["article_id"]);
         return $article;
     }
 
@@ -25,42 +26,93 @@ class ArticleStore extends BaseStore
     {
         $database = $this->database;
         return $database->queryAssoc("
-          SELECT
-            attr.name_de,
-            attr.name_en,
-            attr.name_fr,
-            a2a.value_de,
-            a2a.value_en,
-            a2a.value_fr
-          FROM attribute_article a2a
-            INNER JOIN attribute attr ON a2a.attribute_id = attr.attribute_id
-          WHERE a2a.article_id = :articleId", ["articleId" => $articleId]);
+		SELECT
+		attr.name_de,
+		attr.name_en,
+		attr.name_fr,
+		a2a.value_de,
+		a2a.value_en,
+		a2a.value_fr
+		FROM attribute_article a2a
+		INNER JOIN attribute attr ON a2a.attribute_id = attr.attribute_id
+		WHERE a2a.article_id = :articleId", ["articleId" => $articleId]);
     }
 
     public function getArticleOptions($articleId)
     {
         $database = $this->database;
-        return $database->queryAssoc("
-          SELECT
-            ogr.name_de AS groupname_de,
-            ogr.name_en AS groupname_en,
-            ogr.name_fr AS groupname_fr,
-            opt.name_de,
-            opt.name_en,
-            opt.name_fr,
-            o2a.price,
-            o2a.pseudoprice,
-            o2a.isDefault
-          FROM option_article o2a
-            INNER JOIN option opt ON o2a.option_id = opt.option_id
-            INNER JOIN option_group ogr ON ogr.option_group_id = opt.option_group_id
-          WHERE o2a.article_id = :articleId", ["articleId" => $articleId]);
+        $results = $database->queryAssoc("
+		SELECT
+		ogr.name_de AS groupname_de,
+		ogr.name_en AS groupname_en,
+		ogr.name_fr AS groupname_fr,
+		opt.name_de,
+		opt.name_en,
+		opt.name_fr,
+		o2a.price,
+		o2a.pseudoprice,
+		o2a.isDefault
+		FROM option_article o2a
+		INNER JOIN option opt ON o2a.option_id = opt.option_id
+		INNER JOIN option_group ogr ON ogr.option_group_id = opt.option_group_id
+		WHERE o2a.article_id = :articleId", ["articleId" => $articleId]);
+		
+		$res = array();
+		$resKeys = array();
+		
+		foreach ($results as $result){
+			if(!in_array($result["groupname_de"], $resKeys)){
+				$res[$result["groupname_de"]] = array();
+				array_push($resKeys, $result["groupname_de"]);
+			}
+			array_push($res[$result["groupname_de"]], $result);
+		}
+			
+		return $res;
+    }
+	
+	public function getArticleSets($articleId)
+    {
+        $database = $this->database;
+        $results = $database->queryAssoc("
+		SELECT
+		sets.name_de AS setname_de,
+		sets.name_en AS setname_en,
+		sets.name_fr AS setname_fr,
+		sets.isDefault,
+		ogr.name_de AS groupname_de,
+		ogr.name_en AS groupname_en,
+		ogr.name_fr AS groupname_fr,
+		opt.name_de,
+		opt.name_en,
+		opt.name_fr,
+		o2a.price,
+		o2a.pseudoprice
+		FROM sets
+		INNER JOIN sets_option_article soa ON soa.sets_id = sets.sets_id
+		INNER JOIN option_article o2a ON o2a.option_article_id = soa.option_article_id
+		INNER JOIN option opt ON opt.option_id = o2a.option_id
+		INNER JOIN option_group ogr ON ogr.option_group_id = opt.option_group_id
+		WHERE sets.article_id = :articleId", ["articleId" => $articleId]);
+		
+		$res = array();
+		$resKeys = array();
+		
+		foreach ($results as $result){
+			if(!in_array($result["setname_de"], $resKeys)){
+				$res[$result["setname_de"]] = array();
+				array_push($resKeys, $result["setname_de"]);
+			}
+			array_push($res[$result["setname_de"]], $result);
+		}
+			
+		return $res;
     }
 
     public function getArticle($ordernumber)
     {
         $articleArr = $this->database->queryAssoc("SELECT * FROM article WHERE ordernumber = :ordernumber", ["ordernumber" => $ordernumber]);
-        return $articleArr[0];
+        return $this->getArticleDetails($articleArr[0]);
     }
 
     public function getArticleList($catUrl)
